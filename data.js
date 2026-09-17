00ec2523ec2c1df1bdc66c7ce9dead0994d6860d
nneoum <shfoddlrha@gmail.com>
2026-09-17 20:13:16 +0900

    backup

---
+++ data.js
@@ -0,0 +1,8 @@
+const musicList = [
+  { id: 1, title: "Dynamite", artist: "BTS", album: "BE", releaseDate: "2020-08-21", duration: "3:19", playCount: 34708932, genre: "K-Pop" },
+  { id: 2, title: "Blinding Lights", artist: "The Weeknd", album: "After Hours", releaseDate: "2019-11-29", duration: "3:20", playCount: 50000000, genre: "R&B" },
+  { id: 3, title: "Shape of You", artist: "Ed Sheeran", album: "÷ (Divide)", releaseDate: "2017-01-06", duration: "3:53", playCount: 45000000, genre: "Pop" },
+  { id: 4, title: "Someone Like You", artist: "Adele", album: "21", releaseDate: "2011-01-24", duration: "4:45", playCount: 30000000, genre: "Ballad" },
+  { id: 5, title: "Bohemian Rhapsody", artist: "Queen", album: "A Night at the Opera", releaseDate: "1975-10-31", duration: "5:55", playCount: 20000000, genre: "Rock" },
+  { id: 6, title: "Clair de Lune", artist: "Debussy", album: "Suite Bergamasque", releaseDate: "1905-01-01", duration: "5:00", playCount: 5000000, genre: "Classical" },
+];
